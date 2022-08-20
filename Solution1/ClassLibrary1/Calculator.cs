7a03c4ce07a90500b3d1d1fce2cbb5e23b2c3c2d
suryawanshi-rakesh <rakesh.suryawanshi@accenture.com>
2022-08-20 21:53:39 +0100

    code updated

---
+++ Solution1/ClassLibrary1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public double Add(double num1, double num2) => num1 + num2;
+
+        public double substract(double num1, double num2) => num1 - num2;
+
+        public double multiple(double num1, double num2) => num1 * num2;
+
+        public double divide(double num1, double num2) => num1 / num2 ;
+    }
+}
---
+++ Solution1/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,56 @@
+using ClassLibrary1;
+using NUnit.Framework;
+
+namespace UnitTestProject
+{
+    public class Tests
+    {
+        Calculator calculator; 
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void test_additon()
+        {
+            //arrange
+            int num1 = 100;
+            int num2 = 250;
+            var result = calculator.Add(num1, num2);
+            Assert.AreEqual(result, (num1+num2));
+        }
+
+        [Test]
+        public void test_substraction()
+        {
+            //arrange
+            int num1 = 400;
+            int num2 = 250;
+            var result = calculator.substract(num1, num2);
+            Assert.AreEqual(result, (num1 - num2));
+        }
+
+        [Test]
+        public void test_multiply()
+        {
+            //arrange
+            int num1 = 100;
+            int num2 = 250;
+            var result = calculator.multiple(num1, num2);
+            Assert.AreEqual(result, (num1 * num2));
+        }
+
+        [Test]
+        public void test_divide()
+        {
+            //arrange
+            int num1 = 1000;
+            int num2 = 250;
+            var result = calculator.divide(num1, num2);
+            Assert.AreEqual(result, (num1 / num2));
+        }
+    }
+}
